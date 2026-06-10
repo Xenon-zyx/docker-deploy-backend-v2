8a73203d5cf5f2c699adb0863f52f8540261695d
ZouYaxian20242181 <20242181@stu.nmu.edu.cn>
2026-06-10 08:45:21 +0800

    feat: implement new grade criteria and dual-port deployment configuration

--- src/main/java/camt/se234/deployment/service/GradeServiceImpl.java
+++ src/main/java/camt/se234/deployment/service/GradeServiceImpl.java
@@ -7,17 +7,17 @@ import org.springframework.stereotype.Service;
 public class GradeServiceImpl implements GradeService {
     @Override
     public String getGrade(double score) {
-        if (score > 79.5) {
+        // 💡 我们把新版本的 A/B/C 分数边界调高，作为新旧版本的区分
+        if (score > 89.5) { 
             return "A";
         }
-        else if (score > 74.5){
+        else if (score > 79.5){
             return "B";
-        }else if (score > 59.5){
+        }else if (score > 69.5){
             return "C";
-        }else if (score > 32.5) {
+        }else if (score > 49.5) {
             return "D";
         } else
             return "F";
-
     }
 }
